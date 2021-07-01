f1b0bab566d36a943738991aac45145ca50c4837
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-07-01 20:28:57 +0800

    Added demo api.

--- web/index.php
+++ web/index.php
@@ -22,7 +22,11 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-$app->get('.well-known/apple-app-site-association', function() use($app) {
+$app->get('/demo', function() use($app) {
+  return {'user': 'demo'};
+});
+
+$app->get('/.well-known/apple-app-site-association', function() use($app) {
   return {'user': 'demo'};
 });
 
