8032cfc9c318a70b5a33b6ac39df39cfd09b68f6
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-07-02 10:52:58 +0800

    return static AASA file

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
+	router.StaticFile("/.well-known/apple-app-site-association", "./static/apple-app-site-association")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
@@ -30,18 +31,19 @@ func main() {
 		c.JSON(200, res)
 	})
 
-	router.GET("/.well-known/apple-app-site-association", func(c *gin.Context) {
-		appclips := map[string]interface{}{
-			"apps": []interface{}{
-        		"9HC298K985.com.orlov.cvapp.Clip",
-    		},
-		}
-		result := map[string]interface{}{
-    		"appclips": appclips,
-		}
-		c.Header("Content-Type", "application/json")
-		c.JSON(200, result)
-	})	
+	// router.GET("/.well-known/apple-app-site-association", func(c *gin.Context) {
+	// 	appclips := map[string]interface{}{
+	// 		"apps": []interface{}{
+ //        		"9HC298K985.com.orlov.cvapp.Clip",
+ //    		},
+	// 	}
+	// 	result := map[string]interface{}{
+ //    		"appclips": appclips,
+	// 	}
+
+	// 	c.Header("Content-Type", "application/json")
+	// 	c.JSON(200, result)
+	// })	
 
 	router.Run(":" + port)
 }
